59ecc89436ddb8bc59b349890c61f6fb87c5a17f
DConfer99 <42805989+DConfer99@users.noreply.github.com>
2018-08-29 17:10:19 -0400

    Test.

---
+++ git_test.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>A Git Test</title>
+  </head>
+  <body>
+<h1>Testing out Git</h1>
+</br>
+<p>This is some body text for the test website. I know, I know it's all HTML...
+We're working on PHP stuff, sheesh.</p>
+  </body>
+</html>
