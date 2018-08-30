e976cd7d01c327cd5c60a6f3ed10f024d9ba9c1d
DConfer99 <42805989+DConfer99@users.noreply.github.com>
2018-08-30 10:42:02 -0400

    Test.

--- login.php
+++ login.php
@@ -4,11 +4,24 @@
     <meta charset="utf-8">
     <title></title>
   </head>
+
+<?php
+$username = $_GET['username'];
+$password = $_GET['password'];
+ ?>
+
   <body>
     <form method="get" action="">
-      <input type="text" name="first" placeholder="First"><br/>
-      <input type="text" name="last" value="LAST">
+      <input type="text" name="username" placeholder=" Enter Username"><br />
+      <input type="password" name="password">
+      <br />
       <input type="submit" value="go">
     </form>
+
+    <?php
+echo "Your username was " + $username;
+echo "<br />";
+echo "Your password was $password";
+     ?>
   </body>
 </html>
