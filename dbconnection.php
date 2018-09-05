0f99bf3e3cc666a6a69288d5d04c713bc3e03622
DConfer99 <42805989+DConfer99@users.noreply.github.com>
2018-09-05 11:34:09 -0400

    Test

---
+++ dbconnection.php
@@ -0,0 +1,11 @@
+<?php
+// Setting up the Database Connection
+$db_host = 'localhost'; //database is installed on PHP server
+$db_user = 'dillon'; //name to log into Database
+$db_password = 'southhills#'; //password
+$db_name = 'dillon'; //databse name in mysql
+$conn = new mysqli($db_host, $db_user, $db_password, $db_name);
+if ($conn->connect_error) {
+  die("Connection failed, you dingus: " . $conn->connect_error);
+}
+ ?>
