a0e0b21915e576f6d99bbf57fa265d345caa86c5
DConfer99 <42805989+DConfer99@users.noreply.github.com>
2018-09-05 11:35:00 -0400

    Test

--- dbconnection.php
+++ dbconnection.php
@@ -8,4 +8,7 @@ $conn = new mysqli($db_host, $db_user, $db_password, $db_name);
 if ($conn->connect_error) {
   die("Connection failed, you dingus: " . $conn->connect_error);
 }
+else{
+  "It works!"
+}
  ?>
