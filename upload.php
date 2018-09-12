d60c251064b5e0275b5057dea5f41f2918276d7d
DConfer99 <42805989+DConfer99@users.noreply.github.com>
2018-09-12 11:25:17 -0400

    push

---
+++ upload.php
@@ -0,0 +1,27 @@
+<?php
+if (!isset($_SESSION)){
+  session_start();
+}
+
+if (!isset($_SESSION['username'])){
+  //die("DONTCHU DO IT.");
+  header("Location: login.php');
+}
+
+var_dump($_POST['upload']);
+echo "<hr />";
+var_dump($_FILES['upload']);
+
+if (isset($_FILES['upload'])) {
+  $target_dir = "uploads/";
+  $target_file = $target_dir . basename($_FILES['upload']['name']);
+  move_uploaded_files($_FILES['upload']['temp_name'], $target_file);
+}
+ ?>
+
+ Upload your file:
+ <form action="" method="post" enctype="multipart/form-data"> //need enctype for uploading files
+   <input type="file" name="upload"
+   <br />
+   <input type="submit">
+ </form>
