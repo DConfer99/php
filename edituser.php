b05809d81f63437ad94c62b0930990fbdf2f36b4
DConfer99 <42805989+DConfer99@users.noreply.github.com>
2018-09-25 17:52:40 -0400

    test

--- edituser.php
+++ edituser.php
@@ -19,16 +19,16 @@ if (isset($_GET['id']) && $_GET['edit']=="Edit"){
   $result = $conn->query($sql);
 
 echo "<form action=\"\" method=\"post\">";
-            while ($row = $result->fetch_assoc()){
-              echo "<input name=\"userid\" type =\"text\" disabled value=\"" . $row['userid'] . "\">";
-              echo "<br />";
-              echo "<input name=\"username\" type =\"text\" value=\"" . $row['username'] . "\">";
-              echo "<br />";
-              echo "<input name=\"password\" type =\"text\" disabled value=\"" . $row['userid'] . "\">";
-              echo "<br />";
-              echo "<input name=\"submit\" type=\"submit\" value=\"Change\">;
-            }
-            echo "</form>";
+            // while ($row = $result->fetch_assoc()){
+            //   echo "<input name=\"userid\" type =\"text\" disabled value=\"" . $row['userid'] . "\">";
+            //   echo "<br />";
+            //   echo "<input name=\"username\" type =\"text\" value=\"" . $row['username'] . "\">";
+            //   echo "<br />";
+            //   echo "<input name=\"password\" type =\"text\" disabled value=\"" . $row['userid'] . "\">";
+            //   echo "<br />";
+            //   echo "<input name=\"submit\" type=\"submit\" value=\"Change\">;
+            // }
+            // echo "</form>";
 } else {
 
   echo "You should not be here.";
