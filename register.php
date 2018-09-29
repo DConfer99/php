b95e974ef60912a8a55cb6cf79662519e4798f41
DConfer99 <42805989+DConfer99@users.noreply.github.com>
2018-09-29 19:35:47 -0400

    test

--- register.php
+++ register.php
@@ -1,5 +1,9 @@
 <?php
 
+ if (!isset($_SESSION)){
+   session_start();
+ }
+ 
  //won't contact database when page first loads
  //checks if someone has sent post data
  if ($_SERVER['REQUEST_METHOD'] == 'POST'){
