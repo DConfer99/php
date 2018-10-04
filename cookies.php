a1f40526003329fd15a8b8c2e620996919e05047
DConfer99 <42805989+DConfer99@users.noreply.github.com>
2018-10-04 10:24:05 -0400

    test

--- cookies.php
+++ cookies.php
@@ -12,9 +12,9 @@ $cookie_value = "bob";
    </head>
    <body>
      <?php
-        if (isset($_COOKIE['user']){
-            echo "You have been here before!";
-        }
+        // if (isset($_COOKIE['user']){
+        //     echo "You have been here before!";
+        // }
         // else {
         //     echo "This is your first time here!";
         //     setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/");
