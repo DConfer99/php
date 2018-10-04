ac63a794e2ccd7fcb507f9152517ed647986b524
DConfer99 <42805989+DConfer99@users.noreply.github.com>
2018-10-04 10:16:30 -0400

    test

---
+++ cookies.php
@@ -0,0 +1,23 @@
+<?php
+$cookie_name = "user";
+$cookie_value = "bob";
+setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/");
+// 86400 is number of secinds in a day
+ ?>
+
+ <!DOCTYPE html>
+ <html lang="en" dir="ltr">
+   <head>
+     <meta charset="utf-8">
+     <title></title>
+   </head>
+   <body>
+     <?php
+        if (isset($_COOKIE['user']){
+            echo "You have been here before!";
+        }else{
+            echo "This is your first time here!";
+        }
+      ?>
+   </body>
+ </html>
