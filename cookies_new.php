e148270c03b817a1e09548c9e67519f3598991df
DConfer99 <42805989+DConfer99@users.noreply.github.com>
2018-10-05 01:06:32 -0400

    test

--- cookies_new.php
+++ cookies_new.php
@@ -1,9 +1,12 @@
 <?php
+//sets time zone for page
 date_default_timezone_set("America/New_York");
+
 //set cookie for the date the site was visited last
 $cookie_name = "lastVisitDate";
 $cookie_value = date("F j, Y");
 setcookie($cookie_name, $cookie_value, time() + (86400 * 30));
+
 //set cookie for the time the site was visited last
 $cookie_name = "lastVisitTime";
 $cookie_value = date("g:i A");
@@ -20,8 +23,12 @@ setcookie($cookie_name, $cookie_value, time() + (86400 * 30));
      <h1>Welcome to the cookies page!</h1>
      <p>Contrary to popular belief, you won't actually get a REAL cookie.<br />We just really like storing files on your computer.</p>
      <br />
+
      <?php
-        echo "You last visited this page on " . $_COOKIE['lastVisitDate'] . " at " . $_COOKIE['lastVisitTime'] . ".";
+     if(isset($_COOKIE['lastVisitDate']) && isset($_COOKIE['lastVisitTime'])){
+       echo "You last visited this page on " . $_COOKIE['lastVisitDate'] . " at " . $_COOKIE['lastVisitTime'] . ".";
+     }
       ?>
+      
    </body>
  </html>
