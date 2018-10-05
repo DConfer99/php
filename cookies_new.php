82e29d1c9df27bf356694d9c6372cf8c63dfbe82
DConfer99 <42805989+DConfer99@users.noreply.github.com>
2018-10-05 00:57:02 -0400

    test

--- cookies_new.php
+++ cookies_new.php
@@ -6,11 +6,13 @@ date_default_timezone_set("America/New_York");
 $cookie_name = "lastVisitDate";
 $cookie_value = date("F j, Y");
 setcookie($cookie_name, $cookie_value, time() + (86400 * 30));
+echo $_COOKIE['lastVisitDate'];
 
 //set cookie for the time the site was visited last
 $cookie_name = "lastVisitTime";
 $cookie_value = date("g:i A");
 setcookie($cookie_name, $cookie_value, time() + (86400 * 30));
+echo $_COOKIE['lastVisitTime'];
  ?>
 
  <!DOCTYPE html>
