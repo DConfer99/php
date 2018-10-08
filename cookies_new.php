14bde8d2dee6e04531ee90157052ee407c654c64
DConfer99 <42805989+DConfer99@users.noreply.github.com>
2018-10-08 11:17:47 -0400

    test

--- cookies_new.php
+++ cookies_new.php
@@ -29,7 +29,8 @@ setcookie($cookie_name, $cookie_value, time() + (86400 * 30));
        echo "You last visited this page on " . $_COOKIE['lastVisitDate'] . " at " . $_COOKIE['lastVisitTime'] . ".";
      }
     echo "<br />";
-     echo "<img src=\"images/Cookies-PNG-Photos.png\" width=100px>";
+    echo "<br />";
+     echo "<img src=\"images/Cookies-PNG-Photos.png\" width=300px>";
       ?>
 
    </body>
