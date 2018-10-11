b93d235a79df51d8a43d40c4e434b6da26eab027
DConfer99 <dillonconfer@gmail.com>
2018-10-11 10:25:55 -0400

    test

--- shell_exec.php
+++ shell_exec.php
@@ -1,11 +1,22 @@
 <?php
-$output = shell_exec('ls -lart');
-echo "<pre>$output</pre>";
+// $output = shell_exec('ls -lart');
+// echo "<pre>$output</pre>";
+//
+// $pwd = shell_exec('pwd');
+// echo "<pre>$pwd</pre>";
+//
+// $identity = shell_exec('whoami');
+// echo "<pre>$identity</pre>";
 
-$pwd = shell_exec('pwd');
-echo "<pre>$pwd</pre>";
-
-$identity = shell_exec('whoami');
-echo "<pre>$identity</pre>";
-
+$file_test = file_exists("test");
+if ($file_test){
+  echo "Directory exists!";
+  echo "<br />";
+  echo "Here's what's in it: ";
+  echo "<br />";
+  $testArray = scandir("test/");
+  var_dump($testArray);
+}else{
+  echo "Directory doesn't exist!";
+}
 ?>
