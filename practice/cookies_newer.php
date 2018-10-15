463f9e4456c998f631c923aaedc403d91b8223dc
DConfer99 <dillonconfer@gmail.com>
2018-10-15 11:41:12 -0400

    Test

---
+++ practice/cookies_newer.php
@@ -0,0 +1,39 @@
+<?php
+//sets time zone for page
+date_default_timezone_set("America/New_York");
+//extra comment: the reckoning
+//set cookie containing timestamp for time/date site was last accessed
+$cookie_name = "lastVisitDateTime";
+$cookie_value = mktime();
+setcookie($cookie_name, $cookie_value, time() + (86400 * 30));
+ ?>
+
+ <!DOCTYPE html>
+ <html lang="en" dir="ltr">
+   <head>
+     <meta charset="utf-8">
+     <title></title>
+   </head>
+   <body>
+     <h1>Welcome to the cookies page!</h1>
+     <p>Contrary to popular belief, you won't actually get a REAL cookie.<br />We just really like storing files on your computer.</p>
+     <br />
+
+     <?php
+     //only executes code if user has been to page before
+     if(isset($_COOKIE['lastVisitDateTime'])){
+       echo "You last visited this page on " . date("F j, Y", $_COOKIE['lastVisitDateTime']) . " at " . date("g:i A", $_COOKIE['lastVisitDateTime']) . ".";
+       echo "<br />";
+
+       // subtract current timestamp from previous timestamp to figure out how many seconds have passed since last visit
+       $currentTime = mktime();
+       $secondsPassed = $currentTime - $_COOKIE['lastVisitDateTime'];
+       echo "Why, that was a whole " . $secondsPassed . " seconds ago!";
+     }
+
+    echo "<br />";
+    echo "<br />";
+     echo "<img src=\"images/Cookies-PNG-Photos.png\" width=300px>";
+      ?>
+   </body>
+ </html>
---
+++ practice/shell_exec.php
@@ -0,0 +1,39 @@
+<?php
+// $output = shell_exec('ls -lart');
+// echo "<pre>$output</pre>";
+//
+// $pwd = shell_exec('pwd');
+// echo "<pre>$pwd</pre>";
+//
+// $identity = shell_exec('whoami');
+// echo "<pre>$identity</pre>";
+
+$file_test = file_exists("test");
+if ($file_test){
+  echo "Directory exists!";
+  echo "<br />";
+  echo "Here's what's in it: ";
+  echo "<br />";
+  $testArray = scandir("test/");
+  //var_dump($testArray);
+  foreach ($testArray as $fileName => $value) {
+    if ($value == "." || $value == ".."){continue;}
+    echo $value . "<br />";
+  }
+}else{
+  echo "Directory doesn't exist!";
+}
+
+echo "<br />";
+echo "<br />";
+$users = shell_exec('who');
+$usersArray = explode("\n",$users);
+foreach ($ussersArray as $key => $value) {
+  if ($key = "0" || $key = "1"){continue;}
+  $username = substr($value, 0, strpos($value, ' '));
+  echo $username . "<br />";
+}
+// echo $usersArray[0];
+// echo "<br />";
+// echo "<pre>$users</pre>";
+?>
