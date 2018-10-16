d92a2486150b2546f1985fa5c349e9d3907e40e8
DConfer99 <dillonconfer@gmail.com>
2018-10-15 23:08:13 -0400

    test

--- wcommand.php
+++ wcommand.php
@@ -19,7 +19,8 @@ foreach ($linesArray as $key => $value) {
 //substr() is used to extract username from line
 //strpos() finds the first instance of a space in the line and report location- used in substr() to find cutoff
 echo $value;
-  $userName = substr($value, 0, strpos($value, ' '));
+  //$userName = substr($value, 0, strpos($value, ' '));
+  $userName = substr($value, 0, 6);
   echo $userName . "<br />";
 }
  ?>
