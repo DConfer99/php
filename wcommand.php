779eb4c2719465469040cc63ef120ca4d854c324
DConfer99 <dillonconfer@gmail.com>
2018-10-15 22:36:17 -0400

    test

--- wcommand.php
+++ wcommand.php
@@ -1,7 +1,12 @@
 <?php
 echo "Here is a list of who is currently logged into the 23.30.218.171 server:";
+echo "<br />";
 
 //execute w command, returns string
 $wRawOutput = shell_exec('w');
-echo $wRawOutput;
+//echo $wRawOutput;
+
+//seperates string based on lines, stores in array
+$linesArray = explode("\n",$wRawOutput);
+echo $linesArray[2];
  ?>
