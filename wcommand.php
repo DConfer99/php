2c9d36d46f0f04c2adff68d1e73d1ad3763a0213
DConfer99 <dillonconfer@gmail.com>
2018-10-15 23:05:16 -0400

    test

--- wcommand.php
+++ wcommand.php
@@ -14,7 +14,7 @@ $linesArray = explode("\n",$wRawOutput);
 // goes through lines one by one
 foreach ($linesArray as $key => $value) {
   //skips first two lines (0 and 1) of gabage info
-  if ($key = "0" || $key = "1"){continue;}
+  //if ($key = "0" || $key = "1"){continue;}
 
 //substr() is used to extract username from line
 //strpos() finds the first instance of a space in the line and report location- used in substr() to find cutoff
