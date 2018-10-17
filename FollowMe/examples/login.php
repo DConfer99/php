47fcaed19ac412e39ecec893f984ae8caf3dcc49
DConfer99 <dillonconfer@gmail.com>
2018-10-16 20:18:36 -0400

    test

---
+++ FollowMe/examples/login.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>This is the future site of my login page!</h1>
+    <br/><br/>
+    <p>Now get out.</p>
+  </body>
+</html>
