5bb79b33987fc35038dae5dcaf40a26d0859cb60
DConfer99 <dillonconfer@gmail.com>
2018-10-17 19:35:09 -0400

    test

--- FollowMe/examples/login.php
+++ FollowMe/examples/login.php
@@ -6,8 +6,8 @@ if($_POST['email'] != "" && $_POST['password'] != ""){
 	$email=$_POST['email'];
 	$password=$_POST['password'];
 
-	$sql="SELECT email_addr, password FROM fm_users WHERE email_addr = '$email'";
-	$result=$conn->query($sql);
+	// $sql="SELECT email_addr, password FROM fm_users WHERE email_addr = '$email'";
+	// $result=$conn->query($sql);
 
 	// while ($row = $result->fetch_assoc()){
 	// 	if (password_verify($password, $row['password'])){
