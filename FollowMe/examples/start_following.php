46d9ab1c9cab6e9bb9b9943ba16d74d142468ff5
DConfer99 <dillonconfer@gmail.com>
2018-10-30 10:21:14 -0400

    test

--- FollowMe/examples/start_following.php
+++ FollowMe/examples/start_following.php
@@ -11,7 +11,7 @@ $result=$conn->query($sql);
 $sql = "SELECT following_user_id FROM dillon.fm_follows where user_id=" . $_SESSION['user_id'];
 $following=$conn->query($sql);
 
-//$followingArray;
+$followingArray;
 $followingArray[0]=44;
 $followingArray[1]=55;
 
@@ -20,6 +20,7 @@ while($row = $following->fetch_assoc()){
  $i = 0;
  $followingArray[i] = $row['following_user_id'];
  $i++;
+
 }
 
 echo $followingArray[0];
