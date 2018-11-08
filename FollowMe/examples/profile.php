ebab56e50edc94a7473c46d8cd6893e7d4727a62
DConfer99 <dillonconfer@gmail.com>
2018-11-08 18:19:31 -0500

    test

--- FollowMe/examples/profile.php
+++ FollowMe/examples/profile.php
@@ -166,14 +166,6 @@ $result=$conn->query($sql);
 																					<div class="col-md-7 col-sm-4  ml-auto mr-auto">
 																							<h6><?php echo $first_name; echo " "; echo $last_name;?><br/><small><?php echo $title; ?></small></h6>
 																					</div>
-																					<div class="col-md-3 col-sm-2  ml-auto mr-auto">
-											<div class="form-check">
-																				<label class="form-check-label">
-																						<input class="form-check-input" type="checkbox" value="" checked>
-																						<span class="form-check-sign"></span>
-																				</label>
-																		</div>
-																					</div>
 																			</div>
 																	</li>
 																	<hr />
