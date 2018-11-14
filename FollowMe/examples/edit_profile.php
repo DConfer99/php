1ca8fb22a20d9107f1365716de77c925d4e53fe1
DConfer99 <dillonconfer@gmail.com>
2018-11-14 11:49:14 -0500

    test

--- FollowMe/examples/edit_profile.php
+++ FollowMe/examples/edit_profile.php
@@ -16,7 +16,7 @@ if(isset($_POST['submit'])){
 //echo "\"" . $_FILES['new_image']['name'] . "\"";
 var_dump($_FILES['new_image']);
 
-if (isset($_FILES['new_image'])){
+if ($_FILES['new_image']['name'] != "") {
 	if(!file_exists("./images/" . $_SESSION['user_id'] . "/")){
 		mkdir("./images/" . $_SESSION['user_id'] . "/");
 	}
